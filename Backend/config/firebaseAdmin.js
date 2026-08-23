9bd164e73d1eba406e87e1487cadd6b1c05fad76
Amir Shaikh <skamir2410@gmail.com>
2026-08-23 12:45:36 +0530

    FCM: Typo Fixed

--- Backend/config/firebaseAdmin.js
+++ Backend/config/firebaseAdmin.js
@@ -2,7 +2,7 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 
 const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJEECT_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY
 }
