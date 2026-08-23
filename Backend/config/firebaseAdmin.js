fa3d34161173126583d5152f831280ff050e70ee
Amir Shaikh <skamir2410@gmail.com>
2026-08-23 12:59:30 +0530

    FCM: Change Names of Variables

--- Backend/config/firebaseAdmin.js
+++ Backend/config/firebaseAdmin.js
@@ -2,9 +2,9 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 
 const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY
 }
 
 const firebaseAdmin = initializeApp({ credential: cert(serviceAccount) });
