3e6fe12e6995c1a8ec4ce9d9e2a437638a616e8e
Amir Shaikh <skamir2410@gmail.com>
2026-09-23 21:08:53 +0530

    Tests: For Registration Completed

--- Backend/tests/integration/authTest.js
+++ Backend/tests/integration/authTest.js
@@ -37,7 +37,8 @@ describe("POST /api/auth/register Security & Flow Verification....", () => {
             .send(payload);
     }
 
-    it("Should Register User Successfull with accessToken and refreshToken ...", async () => {
+    // Verify with Valid Credentials
+    it("Should Register User Successfully with accessToken and refreshToken ...", async () => {
         const response = await RegisterUser(testUser)
             .expect(201)
 
@@ -59,76 +60,101 @@ describe("POST /api/auth/register Security & Flow Verification....", () => {
         // Verify Cookies
         const setCookieHeader = response.headers['set-cookie'];
         expect(setCookieHeader).toBeDefined();
-        expect(setCookieHeader[0]).toMatch(/refreshToken=/);
+        expect(setCookieHeader[0]).toMatch(/refreshToken=/i)
         expect(setCookieHeader[0]).toMatch(/HttpOnly/i)
         expect(setCookieHeader[0]).toMatch(/sameSite=Strict/i)
     });
 
+    // Verify admin role
+    it("Should ignore security sensitive fields like role during registration", async () => {
+        await RegisterUser({ ...testUser, role: "admin" }).expect(201)
+
+        const user = await User.findOne({ email: testUser.email })
+        expect(user).not.toBe("admin")
+    });
+
+    // Verify with Duplicate Email
     it("Should Reject and Show User already Exists....", async () => {
         await RegisterUser(testUser)
         const response = await RegisterUser(testUser)
             .expect(403)
 
         expect(response.status).toBe(403)
-        expect(response.body.status).toBe('fail')
-        expect(response.body.message).toBe('User with this email already exist')
+        expect(response.body.status).toMatch(/fail/i)
+        expect(response.body.message).toMatch(/User with this email already exist/i)
     });
 
-    it("Should Reject and Show FirstName must be String Error....", async () => {
-        const response = await RegisterUser({
-            firstName: 123456789,
-            lastName: testUser.lastName,
-            email: testUser.email,
-            password: testUser.password,
-        })
-            .expect(400)
-
-        expect(response.status).toBe(400)
-        expect(response.body.status).toBe('fail')
-        expect(response.body.message).toBe('FirstName can only contain letters and Hyphens (no numbers or spaces).')
-    });
-
-    it("Should Reject and Show FirstName exceeds characters....", async () => {
-        const response = await RegisterUser({
-            firstName: 'AmirShaikh-MohammedAmer',
-            lastName: testUser.lastName,
-            email: testUser.email,
-            password: testUser.password,
-        })
-            .expect(400)
-
-        expect(response.status).toBe(400)
-        expect(response.body.status).toBe('fail')
-        expect(response.body.message).toBe('Name cannot exceed 25 characters.')
-    });
-
+    // Verify with Invalid Email Format
     it("Should Reject and Show Invalid Email Format....", async () => {
         const response = await RegisterUser({
-            firstName: testUser.firstName,
-            lastName: testUser.lastName,
+            ...testUser,
             email: 'skgmail.com',
-            password: testUser.password,
         })
             .expect(400)
 
         expect(response.status).toBe(400)
-        expect(response.body.status).toBe('fail')
-        expect(response.body.message).toBe('Please provide a valid email address')
+        expect(response.body.status).toMatch(/fail/i)
+        expect(response.body.message).toMatch(/Please provide a valid email address/i)
     });
 
+    // Verify Email Storing Process
+    it("Should Normalize email by trimming spaces and covering to lowercase", async () => {
+        await RegisterUser({ ...testUser, email: " Amir@gmail.com " }).expect(201)
+
+        const user = await User.findOne({ email: 'amir@gmail.com' })
+        expect(user).not.toBe(null)
+    });
+
+    // Verify Email Sending with NoSQL Injection
+    it("Should reject non-string query object passed as email (NoSQL Injection)", async () => {
+        await RegisterUser({ ...testUser, email: { "$ne": "" } }).expect(400)
+    });
+
+    // Verify First Name as a number instead of String
+    it("Should Reject and Show FirstName must be String Error....", async () => {
+        const response = await RegisterUser({
+            ...testUser,
+            firstName: 123456789,
+        })
+            .expect(400)
+
+        expect(response.status).toBe(400)
+        expect(response.body.status).toMatch(/fail/i)
+        expect(response.body.message).toBe("FirstName can only contain letters and Hyphens (no numbers or spaces).")
+    });
+
+    // Verify too Long characters in FirstName
+    it("Should Reject and Show FirstName exceeds characters....", async () => {
+        const response = await RegisterUser({
+            ...testUser,
+            firstName: 'AmirShaikh-MohammedAmer',
+        })
+            .expect(400)
+
+        expect(response.status).toBe(400)
+        expect(response.body.status).toMatch(/fail/i)
+        expect(response.body.message).toMatch(/Name cannot exceed 25 characters/i)
+    });
+
+    // verify leaving Password field empty
     it("Should Reject and throw an error for Password Required....", async () => {
         const response = await RegisterUser({
-            firstName: testUser.firstName,
-            lastName: testUser.lastName,
-            email: testUser.email
+            ...testUser,
+            password: " "
         })
             .expect(400)
 
         expect(response.status).toBe(400)
-        expect(response.body.status).toBe('fail')
-        expect(response.body.message).toBe('Please proivde: password')
+        expect(response.body.status).toMatch(/fail/i)
+        expect(response.body.message).toMatch(/Please proivde: password/i)
     });
 
+    it("Should Reject Password shorter than minimum length Requirement", async () => {
+        const response = await RegisterUser({ ...testUser, password: '1234' }).expect(400)
+        expect(response.body.message).toMatch(/Password must be atleast 8 characters/i)
+    });
+
+    // Verify Password hashing
     it("Should Verify is Password Properly Hashed or Not....", async () => {
         const response = await RegisterUser(testUser)
             .expect(201)
@@ -154,14 +180,16 @@ describe("POST /api/auth/login Security & Flow Verification..", () => {
         await createdUser.save();
     });
 
+    const LoginUser = (payload) => {
+        return request(app).post('/api/auth/login').send(payload)
+    }
+
     it("Should issue an accessToken and a secure HttpOnly refreshToken cookie upon a valid credentials", async () => {
-        const response = await request(app)
-            .post("/api/auth/login")
-            .send({ email: testUser.email, password: testUser.password })
-            .expect(200);
+        const response = await LoginUser({ email: testUser.email, password: testUser.password })
+            .expect(200)
 
         // 1. Verifying Response Payload
-        expect(response.body.status).toBe("success");
+        expect(response.body.status).toMatch(/success/i);
         expect(response.body.accessToken).toBeDefined();
         expect(typeof response.body.accessToken).toBe('string');
         expect(response.body.user).toEqual(expect.objectContaining({
@@ -169,41 +197,36 @@ describe("POST /api/auth/login Security & Flow Verification..", () => {
             lastName: testUser.lastName,
             email: testUser.email,
         }));
+
         expect(response.body.password).toBeUndefined(); // Prevent Password Leakage
 
         // 2. Verify Cookie Security Flags
         const setCookieHeader = response.headers["set-cookie"];
         expect(setCookieHeader).toBeDefined();
-        expect(setCookieHeader[0]).toMatch(/refreshToken=/);
+        expect(setCookieHeader[0]).toMatch(/refreshToken=/i);
         expect(setCookieHeader[0]).toMatch(/HttpOnly/i);
         expect(setCookieHeader[0]).toMatch(/SameSite=Strict/i);
     });
 
     it("should reject login attempts with invalid password and return 401 without leaking user existence", async () => {
-        const response = await request(app)
-            .post("/api/auth/login")
-            .send({ email: testUser.email, password: "@Wrongpassword" })
+        const response = await LoginUser({ email: testUser.email, password: "@Wrongpassword" })
             .expect(401);
 
         expect(response.body.status).toBe('fail');
-        expect(response.body.message).toBe("Incorrect email or password");
+        expect(response.body.message).toMatch(/Incorrect email or password/i);
         expect(response.headers["set-cookie"]).toBeUndefined();
     });
 
     it("should detect Compromised token reuse and wipe all user refresh sessions", async () => {
 
         // Step 1: Login to generate a valid refresh token
-        const loginRes = await request(app)
-            .post("/api/auth/login")
-            .send({ email: testUser.email, password: testUser.password });
+        const loginRes = await LoginUser({ email: testUser.email, password: testUser.password })
 
         const validCookie = loginRes.headers["set-cookie"][0];
 
         // Step 2: Use token once to rotate it
-        const refreshRes = await request(app)
-            .post("/api/auth/refresh")
-            .set("Cookie", validCookie)
-            .expect(200);
+        await request(app).post("/api/auth/refresh")
+            .set("Cookie", validCookie).expect(200);
 
         //Step 3: Attempt to reuse the OLD token (simulating an attacker stealing it)
         const attackRes = await request(app)
@@ -211,11 +234,12 @@ describe("POST /api/auth/login Security & Flow Verification..", () => {
             .set("Cookie", validCookie)
             .expect(403);
 
-        expect(attackRes.body.message).toContain("Compromised token usage detected");
+        expect(attackRes.body.message).toMatch(/Compromised token usage detected/i);
 
         // Step 4: Verify the database wiped all Sessions for this user.
         const compromisedUser = await User.findOne({ email: testUser.email }).select('+refreshTokens');
         expect(compromisedUser.refreshTokens.length).toBe(0);
     });
+
 });
 
